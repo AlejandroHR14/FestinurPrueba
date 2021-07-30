399fca8c256454941fedce96fb95c744d7a46f67
AlejandroHR14 <79806695+AlejandroHR14@users.noreply.github.com>
2021-07-30 13:55:27 -0400

    Add files via upload

---
+++ loadingFunction.js
@@ -0,0 +1,16 @@
+
+
+/*function loading() {
+ 
+ $(".div__preload").fadeOut();
+	$(".preload").removeClass('preload');
+ 
+}
+ 
+setTimeout(loading,7000);*/
+
+
+window.onload = function(){
+	$(".div__preload").fadeOut();
+	$(".preload").removeClass('preload');
+}
