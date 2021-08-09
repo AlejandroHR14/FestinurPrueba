7b7fe589fc54f1317364ed468b30ca47ae1407f4
AlejandroHR14 <79806695+AlejandroHR14@users.noreply.github.com>
2021-08-09 12:57:04 -0400

    Add files via upload

--- proyectos/js/index.js
+++ proyectos/js/index.js
@@ -1,4 +1,4 @@
-window.onload = function getData(){
+/*window.onload = function getData(){
 	var xmlhttp; 
 	if(window.XMLHttpRequest){ 
 		xmlhttp = new XMLHttpRequest(); 
@@ -11,7 +11,7 @@ window.onload = function getData(){
 			dividirCadena(linea, '\n');
 		} 
 	} 
-	xmlhttp.open("GET", "https://github.com/AlejandroHR14/FestinurPrueba/blob/master/proyectos/datos/DatosGuardados.txt", true); 
+	xmlhttp.open("GET", "/proyectos/datos/DatosGuardados.txt", true); 
 	xmlhttp.send();
 }
 
@@ -27,4 +27,4 @@ function dividirCadena(cadenaADividir,separador) {
    document.getElementById("video1").src=aa[2];
    //document.getElementById("video1").innerHTML = kk[2];
 }
-//            /proyectos/datos/DatosGuardados.txt
+//            */
