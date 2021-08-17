40cf8c132dc74219f3c56c7c7740176c3421291a
AlejandroHR14 <79806695+AlejandroHR14@users.noreply.github.com>
2021-08-17 19:20:37 -0400

    Add files via upload

---
+++ js/loadingAnimation.js
@@ -0,0 +1,32 @@
+const spans = document.querySelectorAll('.word--animation span');
+
+spans.forEach((span, idx) => {
+	span.addEventListener('click', (e) => {
+		e.target.classList.add('active');
+	});
+	span.addEventListener('animationend', (e) => {
+		e.target.classList.remove('active');
+	});
+	
+	// Initial animation
+	setTimeout(() => {
+		span.classList.add('active');
+	}, 750 * (idx+1))
+});
+
+
+window.onload = function(){
+	$(".div--loading").fadeOut();
+	$(".loading").removeClass('loading');
+	$(".video-h1").fadeIn(2000);
+	$(".scrollDown").fadeIn(2000);
+}
+
+
+
+/*function loading() {
+ 
+	$(".div__preload").fadeOut();
+	$(".preload").removeClass('preload');
+ 
+}setTimeout(loading,7000);*/
---
+++ proyectos/js/index.js
@@ -0,0 +1,30 @@
+/*window.onload = function getData(){
+	var xmlhttp; 
+	if(window.XMLHttpRequest){ 
+		xmlhttp = new XMLHttpRequest(); 
+	} else{ 
+		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP"); 
+	} 
+	xmlhttp.onreadystatechange = function(){ 
+		if(xmlhttp.readyState == 4){ 
+			var linea = xmlhttp.responseText; 
+			dividirCadena(linea, '\n');
+		} 
+	} 
+	xmlhttp.open("GET", "/proyectos/datos/DatosGuardados.txt", true); 
+	xmlhttp.send();
+}
+
+
+
+function dividirCadena(cadenaADividir,separador) {
+   var arrayDeCadenas = cadenaADividir.split(separador);
+   var str = arrayDeCadenas.toString();
+   var aa = str.split(';');
+   
+   document.getElementById("titulo").innerHTML=aa[0];
+   document.getElementById("integrante").innerHTML=aa[1];
+   document.getElementById("video1").src=aa[2];
+   //document.getElementById("video1").innerHTML = kk[2];
+}
+//            */
