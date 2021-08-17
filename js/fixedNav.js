f4d1e4de68fd07a1dc6bd47bff5b5bebf61f5547
AlejandroHR14 <79806695+AlejandroHR14@users.noreply.github.com>
2021-08-17 08:07:47 -0400

    Delete js directory

--- js/fixedNav.js
+++
@@ -1,4 +0,0 @@
-document.querySelector(".show-menu").addEventListener("click", () => {
-  document.getElementById("span").classList.toggle("menu-visible");
-  document.querySelector(".menu-icon").classList.toggle("spin-menu");
-});
